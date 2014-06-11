26fcc9c4baafb260dddd34d3af2d6ce196edf972
fellypeavelino <fellypeavelino@hotmail.com>
2014-06-10 23:57:25 -0300

    primeiro commit

---
+++ WebContent/login.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>
+			Página de Login
+		</title>
+	</head> 
+	<body> 
+		<form action='Ola' method='post'> 
+		<!--
+		 	action= para classe servelet 
+		 -->>
+			Nome.: <input type='text' name='nome' /> 
+			<br /> 
+			Sobrenome.: <input type='text' name='sobrenome' /> 
+			<br /> 
+			<br />
+			<input type='submit' value='Enviar'/> 
+		</form>
+	</body>
+</html>
---
+++ src/jspteste2/Ola.java
@@ -0,0 +1,46 @@
+package jspteste2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ola
+ */
+@WebServlet("/Ola")
+public class Ola extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ola() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nome = request.getParameter("nome"); 
+		String sobrenome = request.getParameter("sobrenome"); 
+		response.setContentType("text/html"); 
+		PrintWriter out = response.getWriter(); 
+		out.println("Bem Vindo<h3>"+nome+" " +sobrenome+"</h3>"); out.close();
+	}
+
+}
